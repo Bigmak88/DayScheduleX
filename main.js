a43a557a648e95ec8f39eb27484703854f0578ed
Mark Kelly <big.mako@gmail.com>
2020-05-31 22:37:14 -0700

    js file created

---
+++ main.js
@@ -0,0 +1,20 @@
+// jQuery ready
+$(document).ready(function () {
+
+//  Create Global Variables
+const m = moment();
+
+//let event = userEvent;
+//let time = userSchedule;
+
+//  Display Current Date & Time
+$('#currentDay').text(m.format('MMMM Do YYYY, h:mm a'));
+//console.log(m.format('MMMM Do YYYY'));
+
+//  Functions for Time to append
+
+//  User Input function
+
+//  Save User input to local storage
+
+});
