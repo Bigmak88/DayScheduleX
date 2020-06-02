2dd58392a56341f5a69998792b69efad8a56b8e9
Mark Kelly <big.mako@gmail.com>
2020-06-02 12:55:53 -0700

    end push

--- main.js
+++ main.js
@@ -4,17 +4,51 @@ $(document).ready(function () {
 //  Create Global Variables
 const m = moment();
 
-//let event = userEvent;
-//let time = userSchedule;
-
 //  Display Current Date & Time
 $('#currentDay').text(m.format('MMMM Do YYYY, h:mm a'));
 //console.log(m.format('MMMM Do YYYY'));
 
-//  Functions for Time to append
+// Color Changing hours to past/present/future
+function colorChange() {
+    var currentTime = moment();
+    console.log("Current Time" + currentTime);
 
-//  User Input function
+    $(".time-block").each(function() {
+        var timeBlock = m.hour(parseInt($(this).attr("hour")));
+        var description = localStorage.getItem($(this).attr('hour'));
 
-//  Save User input to local storage
+        $(this).children('textarea').val(description);
 
+        if (currentTime > timeBlock) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (currentTime < timeBlock) {
+            $(this).addClass("future");
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+        } else {
+            $(this).removeClass("future");
+            $(this).addClass("present");
+            $(this).removeClass("past");
+        }
+        
+console.log('here is hour:'+ " "+ currentTime);
+    });
+}
+    colorChange();
+
+//  Save User input to local storage via SaveBTN
+
+    $(".saveBtn").on('click', function() {
+        event.preventDefault();
+
+        var hour = $(this).parent().attr('hour');
+        var description = $(this).prev().val();
+
+        console.log(description);
+        localStorage.setItem(hour, description);
+        // getting the value of the textarea
+        // save it in localstorage
+    })
 });
